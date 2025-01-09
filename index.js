c63ad514bd421a991f0c07338d9d13712a1477a3
PRASAD KALE <prasadkale1618@gmail.com>
2025-01-09 19:43:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Hello Prasad Kale...! ');
+ res.send('Hello Prasad B Kale...! ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
